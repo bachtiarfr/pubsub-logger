8ed920ddf26bd70abf9cf76a06dcb75450b6a314
bachtiar <bachtiar@privyid.tech>
2023-02-14 14:09:12 +0700

    pubsub: change contract 2

--- main.go
+++ main.go
@@ -10,18 +10,6 @@ import (
 	"github.com/pkg/errors"
 )
 
-type PublisherService interface {
-	PublishReport(ctx context.Context, data entity.Logger) error
-}
-
-type LoggerService struct {
-	publisher *googlepublisher.Client
-}
-
-func NewLoggerService(publisher *googlepublisher.Client) *LoggerService {
-	return &LoggerService{publisher: publisher}
-}
-
 func PublishReport(ctx context.Context, data entity.Logger) error {
 	var r *googlepublisher.Client
 	if r != nil {
