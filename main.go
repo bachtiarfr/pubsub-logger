7f886d0011f9904fef0156b6f2970e3f63fdebc5
bachtiar <bachtiar@privyid.tech>
2023-02-15 17:07:32 +0700

    fixing project structure on v2

--- main.go
+++ main.go
@@ -4,46 +4,10 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"time"
 
 	"cloud.google.com/go/pubsub"
-	"github.com/bachtiarfr/pubsub-logger/v2/internal/entity"
-	"github.com/bachtiarfr/pubsub-logger/v2/pkg/builderx"
-	"github.com/google/uuid"
 	"google.golang.org/api/option"
 )
-
-func main() {
-	ctx := context.Background()
-
-	loggerData := entity.Logger{
-		ID:         	uuid.New(),
-		ServiceName: 	"ServiceName",
-		Request:    	map[string]any{},
-		RequestVendor:  map[string]any{},
-		Response:   	map[string]any{},
-		ResponseVendor: map[string]any{},
-		URL:        	"dummy",
-		UrlVendor:      "dummy",
-		Method:    	 	"POST",
-		StatusCode: 	200,
-		Event:      	"nodeflux_liveness",
-		RequestID:  	"state.RequestID",
-		Additional: 	map[string]any{},
-		Tag:        	"",
-		Error:      	map[string]any{},
-		CreatedAt:  	time.Now().Local(),
-	}
-
-	dataJson, _ := builderx.StructToMap(loggerData, "json")
-
-	errPub := PublishReport(ctx, dataJson)
-	if errPub != nil {
-		fmt.Printf("publish error:", errPub)
-	}
-
-	fmt.Printf("success publish")
-}
 
 func PublishReport(ctx context.Context, data map[string]interface{}) error {
 	d, err := json.Marshal(data)
